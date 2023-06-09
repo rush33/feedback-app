e6e707aa4a47d32d4ca7b897901794f88462212b
Rushad <75556978+rush33@users.noreply.github.com>
2023-06-09 20:50:41 +0530

    Update FeedbackList.jsx

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,28 +1,21 @@
 import React from "react";
 import FeedbackItem from "./feedbackItem";
 import { motion, AnimatePresence } from "framer-motion";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import FeedbackContext from "./context/FeedbackContext";
 
 function FeedbackList() {
-    const [feedbackList, setFeedbackList] = useState([]);
     const {feedback} = useContext(FeedbackContext);
-    useEffect(() => {
-    // This function will run when the feedbackList component is rendered.
-    // It will fetch the feedback items from the backend and update the feedbackList state variable.
-    fetch(`https://important-erin-veil.cyclic.app/feedback?_sort=id&_order=desc`)
-      .then(response => response.json())
-      .then(feedback => setFeedbackList(feedback));
-  }, []);
+    
 
-  if (!feedbackList || feedbackList.length === 0) {
+  if (!feedback || feedback.length === 0) {
     return <p>No feedback yet.</p>;
   }
 
   return (
     <div className="feedback-list">
       <AnimatePresence>
-        {feedbackList.map((item) => (
+        {feedback.map((item) => (
           <motion.div
             key={item.id}
             initial={{ opacity: 0 }}
